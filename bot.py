d9ef909ae4e68d48d96cc9ff38978b787d466abc
fadiasl091-a11y <fadi.asl091@gmail.com>
2026-04-28 00:04:37 +0100

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,28 @@
+from telegram import Update, ReplyKeyboardMarkup
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
+
+TOKEN = "PUT_YOUR_TOKEN"
+
+keyboard = [
+    ["💰 شراء", "📊 الأسعار"],
+    ["📞 تواصل"]
+]
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "مرحبا بك 👋",
+        reply_markup=ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+    )
+
+async def handle(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if update.message.text == "💰 شراء":
+        await update.message.reply_text("ارسل الكمية")
+    elif update.message.text == "📊 الأسعار":
+        await update.message.reply_text("1000 = 1$")
+    elif update.message.text == "📞 تواصل":
+        await update.message.reply_text("@username")
+
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+app.add_handler(MessageHandler(filters.TEXT, handle))
+app.run_polling()
